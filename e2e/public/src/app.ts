ed6b77fc7a503f386a7980b72fc9ee144cb3d0ba
Sergi Yagües <sergiyr90@gmail.com>
2023-01-06 17:08:00 +0100

    e2e: test example update

--- e2e/public/src/app.ts
+++ e2e/public/src/app.ts
@@ -1,6 +1,23 @@
 import { CodeSample } from './../../../src/index'
 
-const codeSample = new CodeSample({})
+const codeSample = new CodeSample({
+  theme: 'nord',
+  samples: {
+    // Id
+    sample: {
+      language: 'javascript',
+      fileName: 'sample.js', // optional
+      content: `
+// This is a comment
+const foo = 'bar';
+for (const item of array) {
+  console.log(item);
+}
+      `
+    }
+  }
+})
 
 console.log(codeSample);
 
+codeSample.attachToElement('.code-sample')
